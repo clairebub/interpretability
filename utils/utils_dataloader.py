177f070abcde082de8b31d5c7fd15662b79e35df
Claire Tang <claire.smurfs@gmail.com>
2019-08-27 01:13:58 -0700

    added utils data processing and loader

--- utils/utils_dataloader.py
+++ utils/utils_dataloader.py
@@ -10,10 +10,15 @@
 # without the express permission of yilin.shen
 # ==================================================
 
+import os
+import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
 
 import torch
 from torchvision import datasets, transforms
+from torch.utils.data.dataset import Dataset
+from PIL import Image
 
 
 class ImageFolderWithPaths(datasets.ImageFolder):
@@ -34,3 +39,25 @@ class ImageFolderWithPaths(datasets.ImageFolder):
 
         return tuple_with_path
 
+
+class MultiLabelDataset(Dataset):
+    def __init__(self, csv_path, img_path, transform=None):
+        tmp_df = pd.read_csv(csv_path)
+
+        self.mlb = MultiLabelBinarizer()
+        self.img_path = img_path
+        self.transform = transform
+
+        self.X_train = tmp_df['image_name']
+        self.y_train = self.mlb.fit_transform(tmp_df['tags'].str.split()).astype(np.float32)
+
+    def __getitem__(self, index):
+        img = Image.open(os.path.join(self.img_path, self.X_train[index]))
+        img = img.convert('RGB')
+        if self.transform is not None:
+            img = self.transform(img)
+        label = torch.from_numpy(self.y_train[index])
+        return img, label
+
+    def __len__(self):
+        return len(self.X_train.index)
